4670d8d42147ff17aa1052997dd2431cbad83e5c
jannik <j.upmann@googlemail.com>
2023-02-05 18:07:31 +0100

    Adding main Programm Flow

--- src/main/java/de/upmann/Main.java
+++ src/main/java/de/upmann/Main.java
@@ -1,7 +1,43 @@
 package de.upmann;
 
+import de.upmann.repositories.CSVRepository;
+import de.upmann.repositories.MockUpRepository;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("-----------------------------------------");
+        System.out.println("------------SuperDuperMarkt--------------");
+        System.out.println("---------von Jannik Upmann---------------");
+        System.out.println("-----------------------------------------");
+        System.out.println("Herzlich Willkommen im SuperDuperMarkt. Bitte wählen Sie jetzt Ihre Datenquelle: ");
+        System.out.println("[1/standard]MockUp [2]CSV [3]SQlite");
+        Scanner eingabe = new Scanner(System.in);
+        int datenquellenNummer = eingabe.nextInt();
+        ProductRepository productRepository;
+        if(datenquellenNummer == 2){
+            productRepository = new CSVRepository("markt.csv");
+            productRepository.updateProducts(new MockUpRepository().getProducts());
+        }
+        else {
+            productRepository = new MockUpRepository();
+        }
+        Rack rack = new Rack(productRepository);
+
+        while (true){
+            System.out.println("Schreiben Sie \"next\" für den nächsten Tag");
+            String next = eingabe.next();
+            if(next.equals("exit")){
+                break;
+            }
+            if(next.equals("next")){
+                rack.increasDay();
+            }
+
+
+
+        }
+
     }
 }
